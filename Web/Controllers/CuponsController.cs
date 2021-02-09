e8139029e3488a95c96c192fb0bf4b1e886d336c
dgsmidt <daniel.smidt@yahoo.com.br>
2021-02-09 18:26:25 -0300

    Ok

---
+++ Web/Controllers/CuponsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DAL;
+using DAL.Models;
+
+namespace Web.Controllers
+{
+    public class CuponsController : Controller
+    {
+        private readonly UpFirstDbContext _context;
+
+        public CuponsController(UpFirstDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cupons
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cupons.ToListAsync());
+        }
+
+        // GET: Cupons/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cupom = await _context.Cupons
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cupom == null)
+            {
+                return NotFound();
+            }
+
+            return View(cupom);
+        }
+
+        // GET: Cupons/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cupons/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Codigo,Data,Validade,Desconto,Utilizado")] Cupom cupom)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cupom);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cupom);
+        }
+
+        // GET: Cupons/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cupom = await _context.Cupons.FindAsync(id);
+            if (cupom == null)
+            {
+                return NotFound();
+            }
+            return View(cupom);
+        }
+
+        // POST: Cupons/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Codigo,Data,Validade,Desconto,Utilizado")] Cupom cupom)
+        {
+            if (id != cupom.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cupom);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CupomExists(cupom.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cupom);
+        }
+
+        // GET: Cupons/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cupom = await _context.Cupons
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cupom == null)
+            {
+                return NotFound();
+            }
+
+            return View(cupom);
+        }
+
+        // POST: Cupons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cupom = await _context.Cupons.FindAsync(id);
+            _context.Cupons.Remove(cupom);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CupomExists(int id)
+        {
+            return _context.Cupons.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Web/Controllers/DescontosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DAL;
+using DAL.Models;
+
+namespace Web.Controllers
+{
+    public class DescontosController : Controller
+    {
+        private readonly UpFirstDbContext _context;
+
+        public DescontosController(UpFirstDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Descontos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Descontos.ToListAsync());
+        }
+
+        // GET: Descontos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var desconto = await _context.Descontos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (desconto == null)
+            {
+                return NotFound();
+            }
+
+            return View(desconto);
+        }
+
+        // GET: Descontos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Descontos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Descricao,Valor")] Desconto desconto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(desconto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(desconto);
+        }
+
+        // GET: Descontos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var desconto = await _context.Descontos.FindAsync(id);
+            if (desconto == null)
+            {
+                return NotFound();
+            }
+            return View(desconto);
+        }
+
+        // POST: Descontos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Descricao,Valor")] Desconto desconto)
+        {
+            if (id != desconto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(desconto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DescontoExists(desconto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(desconto);
+        }
+
+        // GET: Descontos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var desconto = await _context.Descontos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (desconto == null)
+            {
+                return NotFound();
+            }
+
+            return View(desconto);
+        }
+
+        // POST: Descontos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var desconto = await _context.Descontos.FindAsync(id);
+            _context.Descontos.Remove(desconto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DescontoExists(int id)
+        {
+            return _context.Descontos.Any(e => e.Id == id);
+        }
+    }
+}
